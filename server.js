702a70685a89afaf0b9bdf86ecb974b2509bf635
wabrodom <113647599+wabrodom@users.noreply.github.com>
2023-04-27 08:11:35 +0700

    remove cors on app.get that not neccessary

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", cors(), (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
